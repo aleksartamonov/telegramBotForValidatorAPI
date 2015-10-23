db6453da3c36119e9b0ceca3ee74b41f6e3faa5b
aleksartamonov <aleksartamonov@gmail.com>
2015-10-23 17:23:59 +0300

    test str

--- __init__.py
+++ __init__.py
@@ -27,6 +27,7 @@ CERT     = '/home/aleksart/openssl/sert.pem'
 CERT_KEY = '/home/aleksart/openssl/sert.key'
 
 print(TOKEN)
+print(test)
 bot = TelegramBot(TOKEN)
 app = Flask(__name__)
 context = (CERT, CERT_KEY)
